d4331e5e3edc028791bc1b29e70f413e0a3a0f62
zignis <velociraptorm77@gmail.com>
2023-12-22 19:38:02 +0530

    .

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -1,9 +1,10 @@
 /* eslint-disable prefer-snakecase/prefer-snakecase */
 
+import { CaptureConsole } from "@sentry/integrations";
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://563fb5e46f4cb639c7f8c839f4046416@o4506393718554624.ingest.sentry.io/4506393724977152",
-  integrations: [],
+  integrations: [new CaptureConsole({ levels: ["error"] })],
   tracesSampleRate: 0.8
 });
