b6773c27272c9941f38e747212a064f721366b4a
zignis <velociraptorm77@gmail.com>
2023-12-25 00:26:18 +0530

    .

--- apps/web/src/app/preload-resources.tsx
+++ apps/web/src/app/preload-resources.tsx
@@ -2,13 +2,18 @@
 
 import ReactDOM from "react-dom";
 
+const ASSETS_URL = process.env.NEXT_PUBLIC_ASSETS_URL || "";
 const CDN_URL = process.env.NEXT_PUBLIC_CDN_URL || "";
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
 
 export const PreloadResources = (): null => {
+  ReactDOM.preconnect(ASSETS_URL);
+  ReactDOM.prefetchDNS(ASSETS_URL);
+
   ReactDOM.preconnect(CDN_URL);
-  ReactDOM.preconnect(API_URL);
   ReactDOM.prefetchDNS(CDN_URL);
+
+  ReactDOM.preconnect(API_URL);
   ReactDOM.prefetchDNS(API_URL);
 
   return null;
