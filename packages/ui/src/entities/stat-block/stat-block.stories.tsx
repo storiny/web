02c1b5f97747bda9396cb172d033cf3c867d552a
zignis <velociraptorm77@gmail.com>
2024-01-08 23:24:45 +0530

    .

--- packages/ui/src/entities/stat-block/stat-block.stories.tsx
+++ packages/ui/src/entities/stat-block/stat-block.stories.tsx
@@ -1,6 +1,9 @@
 // noinspection JSUnusedGlobalSymbols
 
 import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+
+import RectangleIcon from "~/icons/rectangle";
 
 import StatBlock from "./stat-block";
 
@@ -9,9 +12,7 @@ const meta: Meta<typeof StatBlock> = {
   component: StatBlock,
   args: {
     value: "Value",
-    label: "Label",
-    caption: "Caption",
-    caption_icon: "increment"
+    label: "Label"
   },
   tags: ["autodocs"]
 };
@@ -20,3 +21,30 @@ export default meta;
 type Story = StoryObj<typeof StatBlock>;
 
 export const Default: Story = {};
+
+export const WithCaption: Story = {
+  args: {
+    caption: "Caption"
+  }
+};
+
+export const WithIncrementCaptionIcon: Story = {
+  args: {
+    caption: "Increment",
+    caption_icon: "increment"
+  }
+};
+
+export const WithDecrementCaptionIcon: Story = {
+  args: {
+    caption: "Decrement",
+    caption_icon: "decrement"
+  }
+};
+
+export const WithCustomCaptionIcon: Story = {
+  args: {
+    caption: "Custom",
+    caption_icon: <RectangleIcon />
+  }
+};
