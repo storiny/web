b1de634c4a88825d1ed9c2f00113c08cc33722e2
zignis <velociraptorm77@gmail.com>
2024-01-18 00:24:50 +0530

    improve workflows

--- apps/discovery/src/request.rs
+++ apps/discovery/src/request.rs
@@ -1,6 +1,6 @@
 use lazy_static::lazy_static;
 
-pub static USER_AGENT: &'static str = "storiny-bot/1.0";
+pub static USER_AGENT: &str = "storiny-bot/1.0";
 
 lazy_static! {
     pub static ref REQUEST_CLIENT: reqwest::Client = reqwest::Client::new();
