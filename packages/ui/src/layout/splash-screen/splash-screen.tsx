5e47627da4302681ae235feecab5bc669fbe74a7
zignis <velociraptorm77@gmail.com>
2024-02-09 16:03:45 +0530

    fix splash screen

--- packages/ui/src/layout/splash-screen/splash-screen.tsx
+++ packages/ui/src/layout/splash-screen/splash-screen.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { animated, useTransition as use_transition } from "@react-spring/web";
 import clsx from "clsx";
 import React from "react";
 
@@ -11,46 +10,28 @@ import styles from "./splash-screen.module.scss";
 import { SplashScreenProps } from "./splash-screen.props";
 
 const SplashScreen = (props: SplashScreenProps): React.ReactElement | null => {
-  const {
-    className,
-    style: style_prop,
-    force_mount,
-    children,
-    ...rest
-  } = props;
+  const { className, force_mount, children, ...rest } = props;
   const [visible, set_visible] = React.useState<boolean>(true);
-  const [transitions, api] = use_transition(visible, () => ({
-    config: { duration: 150 },
-    from: { opacity: 1, transform: "scale(1)" },
-    enter: { opacity: 1, transform: "scale(1)" },
-    leave: { opacity: 0, transform: "scale(1.25)" }
-  }));
 
   React.useLayoutEffect(() => {
     set_visible(false);
+  }, []);
 
-    if (!force_mount) {
-      setTimeout(api.start);
-    }
-  }, [api, force_mount]);
+  if (!visible && !force_mount) {
+    return null;
+  }
 
-  return transitions(
-    (style, show) =>
-      show && (
-        <animated.div
-          {...rest}
-          aria-label={"Loading…"}
-          className={clsx(css["flex-col"], styles["splash-screen"], className)}
-          style={{ ...style_prop, ...style }}
-        >
-          <Logo size={64} />
-          {children && (
-            <div className={clsx(css["flex-col"], styles.children)}>
-              {children}
-            </div>
-          )}
-        </animated.div>
-      )
+  return (
+    <div
+      {...rest}
+      aria-label={"Loading…"}
+      className={clsx(css["flex-col"], styles["splash-screen"], className)}
+    >
+      <Logo size={64} />
+      {children && (
+        <div className={clsx(css["flex-col"], styles.children)}>{children}</div>
+      )}
+    </div>
   );
 };
 
