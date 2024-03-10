0ca270fca09f408818dc6d34e493c3cd8cd44c28
zignis <velociraptorm77@gmail.com>
2024-03-10 22:15:58 +0530

    add <BlogMemberRequest />

---
+++ packages/ui/src/entities/blog-member-request/blog-member-request.test.tsx
@@ -0,0 +1,30 @@
+import { axe } from "@storiny/test-utils";
+import { FriendRequest as TFriendRequest } from "@storiny/types";
+import React from "react";
+
+import { render_test_with_provider } from "~/redux/test-utils";
+
+import { TEST_USER } from "../../mocks";
+import FriendRequest from "./blog-member-request";
+
+const TEST_FRIEND_REQUEST: TFriendRequest = {
+  user: TEST_USER,
+  created_at: "2022-05-18T01:07:02.000Z",
+  id: "0"
+};
+
+describe("<FriendRequest />", () => {
+  it("renders", () => {
+    render_test_with_provider(
+      <FriendRequest friend_request={TEST_FRIEND_REQUEST} />
+    );
+  });
+
+  it("does not have any accessibility violations", async () => {
+    const { container } = render_test_with_provider(
+      <FriendRequest friend_request={TEST_FRIEND_REQUEST} />
+    );
+
+    expect(await axe(container)).toHaveNoViolations();
+  });
+});
