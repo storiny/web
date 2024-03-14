056a21a8fe6dd87f565e278dc8ee4ce71a883edc
zignis <velociraptorm77@gmail.com>
2024-03-14 23:49:04 +0530

    add blog follower tests

---
+++ apps/backend/tests/tables/blog_followers/fixtures/blog.sql
@@ -0,0 +1,11 @@
+WITH inserted_user AS (
+	INSERT INTO
+		users (name, username, email)
+		VALUES ('Blog owner', 'blog_owner', 'blog_owner@example.com')
+		RETURNING id
+					  )
+INSERT
+INTO
+	blogs (id, name, slug, user_id)
+VALUES (3, 'Sample blog', 'sample-blog', (SELECT id FROM inserted_user));
+
