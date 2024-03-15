d0e22d623049ade655a095a6233a593e09adc78f
zignis <velociraptorm77@gmail.com>
2024-03-15 13:12:42 +0530

    add blog trigger

---
+++ apps/backend/migrations/20240315072650_create_blog_trigger.down.sql
@@ -0,0 +1,11 @@
+DROP TRIGGER IF EXISTS blog_before_insert_trigger ON blogs;
+
+DROP FUNCTION IF EXISTS blog_before_insert_trigger_proc;
+
+DROP TRIGGER IF EXISTS blog_after_insert_trigger ON blogs;
+
+DROP FUNCTION IF EXISTS blog_after_insert_trigger_proc;
+
+DROP TRIGGER IF EXISTS blog_update_trigger ON blogs;
+
+DROP FUNCTION IF EXISTS blog_update_trigger_proc;
