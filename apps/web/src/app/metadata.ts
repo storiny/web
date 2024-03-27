7b3dbbe0a25f0c32dd90966f083ed2c1e20c41a3
zignis <velociraptorm77@gmail.com>
2024-03-27 14:42:15 +0530

    fix analytics

--- apps/web/src/app/metadata.ts
+++ apps/web/src/app/metadata.ts
@@ -26,9 +26,8 @@ export const viewport: Viewport = {
 export const metadata: Metadata = {
   /* eslint-disable prefer-snakecase/prefer-snakecase */
   metadataBase: new URL(process.env.NEXT_PUBLIC_WEB_URL!),
-
   description,
-  manifest: "/app.webmanifest",
+  manifest: `${process.env.NEXT_PUBLIC_WEB_URL}/app.webmanifest`,
   openGraph: {
     title: "Storiny – Share your story",
     description,
@@ -58,77 +57,77 @@ export const metadata: Metadata = {
     statusBarStyle: "default",
     startupImage: [
       {
-        url: "/splash_images/apple-splash-2048-2732.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-2048-2732.jpg`,
         media:
           "(width: 1024px) and (height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-1668-2388.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-1668-2388.jpg`,
         media:
           "(width: 834px) and (height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-1536-2048.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-1536-2048.jpg`,
         media:
           "(width: 768px) and (height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-1668-2224.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-1668-2224.jpg`,
         media:
           "(width: 834px) and (height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-1620-2160.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-1620-2160.jpg`,
         media:
           "(width: 810px) and (height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-1290-2796.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-1290-2796.jpg`,
         media:
           "(width: 430px) and (height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-1179-2556.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-1179-2556.jpg`,
         media:
           "(width: 393px) and (height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-1284-2778.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-1284-2778.jpg`,
         media:
           "(width: 428px) and (height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-1170-2532.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-1170-2532.jpg`,
         media:
           "(width: 390px) and (height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-1125-2436.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-1125-2436.jpg`,
         media:
           "(width: 375px) and (height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-1242-2688.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-1242-2688.jpg`,
         media:
           "(width: 414px) and (height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-828-1792.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-828-1792.jpg`,
         media:
           "(width: 414px) and (height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-1242-2208.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-1242-2208.jpg`,
         media:
           "(width: 414px) and (height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-750-1334.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-750-1334.jpg`,
         media:
           "(width: 375px) and (height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
       },
       {
-        url: "/splash_images/apple-splash-640-1136.jpg",
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}/splash_images/apple-splash-640-1136.jpg`,
         media:
           "(width: 320px) and (height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
       }
--- apps/web/src/app/ph-provider.tsx
+++ apps/web/src/app/ph-provider.tsx
@@ -4,17 +4,11 @@ import posthog from "posthog-js";
 import { PostHogProvider as PostHogProviderPrimitive } from "posthog-js/react";
 import React from "react";
 
-if (typeof window !== "undefined") {
+if (typeof window !== "undefined" && window.origin === "https://storiny.com") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     ui_host: "https://app.posthog.com",
-    capture_pageview: false,
-    loaded: (ph) => {
-      // Only track events on storiny.com
-      if (window.origin !== "https://storiny.com") {
-        ph.opt_out_capturing();
-      }
-    }
+    capture_pageview: false
   });
 }
 
