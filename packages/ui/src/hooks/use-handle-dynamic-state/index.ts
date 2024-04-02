398c2538a6c94973f896099c073648698a339253
zignis <velociraptorm77@gmail.com>
2024-04-03 00:27:44 +0530

    fix dynamic state handle

--- packages/ui/src/hooks/use-handle-dynamic-state/index.ts
+++ packages/ui/src/hooks/use-handle-dynamic-state/index.ts
@@ -1,9 +1,10 @@
 "use client";
 
+import { usePathname as use_pathname } from "next/navigation";
 import React from "react";
 
 /**
- * Resets the state to its initial value on mount. Used during dynamic navigation.
+ * Resets the state to its initial value on navigation.
  * @param initial_value The initial value of the state.
  * @param dispatch The state dispatcher.
  */
@@ -11,15 +12,9 @@ export const use_handle_dynamic_state = <T>(
   initial_value: T,
   dispatch: React.Dispatch<React.SetStateAction<T>>
 ): void => {
-  const mounted_ref = React.useRef(false);
+  const pathname = use_pathname();
 
-  // Reset page on unmount
   React.useEffect(() => {
-    // Do not run on the first mount.
-    if (!mounted_ref.current) {
-      mounted_ref.current = true;
-    } else {
-      dispatch(initial_value);
-    }
-  }, [dispatch, initial_value]);
+    dispatch(initial_value);
+  }, [dispatch, initial_value, pathname]);
 };
