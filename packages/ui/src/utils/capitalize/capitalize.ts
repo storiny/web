d76b69626c7338b096b8f7847120d7c2fcdb3c88
zignis <velociraptorm77@gmail.com>
2024-04-02 14:08:01 +0530

    fix theme

--- packages/ui/src/utils/capitalize/capitalize.ts
+++ packages/ui/src/utils/capitalize/capitalize.ts
@@ -2,5 +2,5 @@
  * Capitalizes the first letter of a string
  * @param str The string to capitalize
  */
-export const capitalize = (str: string): string =>
+export const capitalize = (str = ""): string =>
   str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
