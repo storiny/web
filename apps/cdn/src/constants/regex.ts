3baa00a700b1e1f8c506f1d200325d96346a5246
zignis <velociraptorm77@gmail.com>
2024-04-05 16:59:26 +0530

    fix cdn thumb segment

--- apps/cdn/src/constants/regex.ts
+++ apps/cdn/src/constants/regex.ts
@@ -8,6 +8,8 @@ export const NATIVE_REGEX = new RegExp(
   `\\/(?:(uploads|dl)\\/)?(?:(w@(?:auto|${VALID_SIZES}))\\/)?((?!uploads|dl)[\\/a-zA-Z0-9._-]+)$`
 );
 
+export const THUMB_REGEX = new RegExp(`\\/thumb\\/([a-zA-Z0-9_-]+)$`);
+
 export const REMOTE_REGEX = new RegExp(
   `\\/remote\\/(?:(w@(?:auto|${VALID_SIZES}))\\/)?([a-zA-Z0-9_-]+)\\/([a-zA-Z0-9._-]+)$`
 );
