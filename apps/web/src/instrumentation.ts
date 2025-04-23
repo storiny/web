33125a1e949f4bd5d04deca0dcb95fdc91a6ad9d
zignis <velociraptorm77@gmail.com>
2025-04-23 23:09:32 +0530

    fix sentry

--- apps/web/src/instrumentation.ts
+++ apps/web/src/instrumentation.ts
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/nextjs";
+
 export const register = async (): Promise<void> => {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("../sentry.server.config");
@@ -7,3 +9,6 @@ export const register = async (): Promise<void> => {
     await import("../sentry.edge.config");
   }
 };
+
+// eslint-disable-next-line prefer-snakecase/prefer-snakecase
+export const onRequestError = Sentry.captureRequestError;
